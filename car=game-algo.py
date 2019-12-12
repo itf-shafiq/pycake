8a14d47efa3aedd87c8802660156b883380468e7
Shafiq <itf.shafiq@gmail.com>
2019-12-13 00:45:36 +0600

    unfinished

---
+++ car=game-algo.py
@@ -0,0 +1,10 @@
+# Write algorithom to stata a car game and stop 
+
+x = 0
+start = input("please write 'start' to stat the game")
+
+if start == start:
+    while x == 0:
+        print("pi pi pi")
+else:
+    input("please write the correct spelling")
