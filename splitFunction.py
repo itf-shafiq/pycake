3fa5e9c83d160019f847e54397f082e38100c78d
Shafiq <itf.shafiq@gmail.com>
2020-01-02 13:34:13 +0600

    Testing use of split function

---
+++ splitFunction.py
@@ -0,0 +1,17 @@
+'''a, b = input("Enter any number you want").split()
+
+print( a , b)
+print(type(a))
+print(type(b))'''
+
+
+
+"""a,b = map(int,input("Enter Any Number You Want: ").split())
+
+
+
+print(a*b)"""
+
+a,b = map(int,input("Enter any Number: ").split())
+
+print("Multiplication of a and b is: " + str(a*b))
